43dfe361d1a35a35b3d9f729fe5ffc93159d243a
Victor Sharamet <sharamet@gmail.com>
2018-07-24 21:14:50 +0300

    sass task completed

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
-gulp.task('server', ['sass'], function() {
+gulp.task('server', ['styles'], function() {
     browserSync.init({
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    gulp.watch('./app/sass/**/*.scss', ['sass']);
+    gulp.watch('./app/sass/**/*.scss', ['styles']);
 });
 
-gulp.task('sass', function() {
+gulp.task('styles', function() {
     return gulp.src('./app/sass/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./app/css'))
