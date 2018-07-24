9d15fadc42f23128c079a1a884bbbf52aee948ad
Victor Sharamet <sharamet@gmail.com>
2018-07-24 20:53:01 +0300

    sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var less = require('gulp-less');
 var sass = require('gulp-sass');
 
 gulp.task('server', ['sass'], function() {
@@ -8,15 +7,7 @@ gulp.task('server', ['sass'], function() {
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
-    // gulp.watch('./app/less/**/*.less', ['less']);
     gulp.watch('./app/sass/**/*.scss', ['sass']);
-});
-
-gulp.task('less', function() {
-    return gulp.src('./app/less/**/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('./app/css'))
-    .pipe(browserSync.stream());
 });
 
 gulp.task('sass', function() {
